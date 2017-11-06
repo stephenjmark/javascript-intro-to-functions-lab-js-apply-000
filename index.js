85186053bccdfdfbdc7a477363481b0bcf9faff3
Stephen Mark <stephenjmark@gmail.com>
2017-11-06 06:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ function sayHiToGrandma(string){
   var lowercase = "hello!"
   var mixedCase = "I love you, Grandma."
 
-  if (string === uppercase) {
-      return "YES INDEED!"
-  }
   if (string === lowercase) {
-      return "I can't hear you!"
+      return 'I can\'t hear you!'
   }
 
+  if (string === uppercase) {
+      return 'YES INDEED!'
+  }
+  
   if (string === mixedCase) {
       return "I love you, too."
   }
