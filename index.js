b18b48b04222f3bc6ae049374bb03ff804c7c083
Stephen Mark <stephenjmark@gmail.com>
2017-11-06 07:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sayHiToGrandma(string){
       return 'I can\'t hear you!'
   }
 
-  if (string === string.uppercase) {
+  if (string === string.toUpperCase()) {
       return 'YES INDEED!'
   }
   
