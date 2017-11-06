5bc30313e04c01c89e370d1acb60492c8d16b044
Stephen Mark <stephenjmark@gmail.com>
2017-11-06 06:44:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+  return string.toUpperCase()
+
+}
+
+function whisper(string){
+  return string.toLowerCase()
+
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO!"
+  var lowercase = "hello!"
+  var mixedCase = "Hello!"
+
+  if (string === lowercase) {
+      return "I can't hear you!"
+  }
+  if (string === uppercase) {
+      return "YES INDEED!"
+  }
+  if (string === mixedCase) {
+      return "I love you, too!"
+  }
+}
